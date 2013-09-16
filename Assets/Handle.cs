35d60195cb1f042820c7e29cd9e1bbaead0bd229
Mat Teubert <mathew.teubert@pixelbreakfast.com>
2013-09-16 13:20:20 -0700

    Content Included
    Added existing content to initial repo files

---
+++ Assets/Handle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Handle : MonoBehaviour {
+	public Transform target;
+    GameObject parent;
+
+
+    void Start () {
+        parent = transform.parent.gameObject;
+        parent.animation["grow"].speed = 0;
+	}
+		
+    // Update is called once per frame
+	void Update () {
+
+        transform.position = target.position;
+    }
+
+    void OnMouseDown()
+    {
+
+        parent.animation["grow"].time = parent.animation["grow"].length;
+        parent.animation["grow"].speed = 0;
+    } 
+
+}
+
